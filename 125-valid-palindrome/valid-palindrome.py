81c935ed2a5829a4c116ae1429ab431040a467c2
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2024-04-12 10:51:50 -0400

    Time: 45 ms (56.67%) | Memory: 17.7 MB (30.47%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,16 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s=s.translate(str.maketrans('', '', string.punctuation))
-        s=s.replace(" ","")
-        s=s.lower()
-        left=0
-        right=len(s)-1
-
-        while left<right:
-            if s[left].lower()!=s[right].lower():
-                return False
-
-            left+=1
-            right-=1
-        return True
-            
+        lower_case = s.lower()
+        res = "".join(sym for sym in lower_case if sym.isalnum())
+        if res == res[::-1]:
+            return True
+        return False
