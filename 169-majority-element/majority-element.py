d64bd27580d950a781ba429225f224c05555c7e7
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2024-08-03 11:45:54 -0400

    Time: 171 ms (54.90%) | Memory: 18.1 MB (16.20%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,6 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         counter = collections.Counter(nums)
+        currentMax = 0
+        majorityElement = -1
         for i in counter:
-            if counter[i] > len(nums)//2:
-                return i
+            if counter[i] > currentMax:
+                currentMax = counter[i]
+                majorityElement = i
+
+        return majorityElement
