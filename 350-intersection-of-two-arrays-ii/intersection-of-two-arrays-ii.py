ff1b99363e52cf47efab0193ff63f758c98ba6f0
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2024-08-13 13:47:26 -0700

    Time: 41 ms (91.04%) | Memory: 16.7 MB (25.91%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,17 +1,11 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        minLen = min(len(nums1),len(nums2))
-        res=[]
-        counter = collections.Counter(nums1 if minLen == len(nums1) else nums2)
-        tempCounter = collections.Counter(nums1 if minLen == len(nums1) else nums2)
-        iterator = nums2 if minLen == len(nums1) else nums1
-        for i in iterator:
-            if i in counter and counter[i]>0:
-                counter[i]-=1
-
-        for i in tempCounter:
-            print(tempCounter)
-            if counter[i]!=tempCounter[i]:
-                for x in range(tempCounter[i]-counter[i]):
-                    res.append(i)
-        return res
+        dct = Counter(nums1)
+        result = []
+            
+        for n2 in nums2:
+            if n2 in dct and dct[n2]>0:
+                result.append(n2)
+                dct[n2] -= 1
+                
+        return result
