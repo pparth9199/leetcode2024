52d60684f99a57a710a41b9aa407149de89730f6
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2024-08-13 22:16:08 -0700

    Time: 27 ms (97.06%) | Memory: 16.5 MB (66.00%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,17 +1,13 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        numbers=set()
-        number=0
-        while True:
-            for i in str(n):
-                number+=int(i)**2
-
-            if number==1:
-                return True
-            if number in numbers:
-                return False
-            numbers.add(number)
-            n=number
-            number=0
-
-            
+        def get_next(number: int) -> int:
+            return sum(int(i) ** 2 for i in str(number))
+        
+        slow = n
+        fast = get_next(n)
+        
+        while fast != 1 and slow != fast:
+            slow = get_next(slow)          # Moves one step
+            fast = get_next(get_next(fast)) # Moves two steps
+        
+        return fast == 1
