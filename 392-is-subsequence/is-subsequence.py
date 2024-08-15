0975a98b265a5815b74c22eeb15128f8a76cd7d8
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2024-08-15 16:27:19 -0700

    Time: 35 ms (61.91%) | Memory: 16.6 MB (62.48%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,8 +1,7 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         l,r=0,0
-        if not s:
-            return True
+
         if len(s)>len(t):
             return False
         while r<len(t) and l<len(s):
