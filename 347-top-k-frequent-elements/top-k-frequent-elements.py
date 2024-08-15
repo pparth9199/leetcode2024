4932e2c96fa646d0bf471b9ac5a11aca6eb1275a
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2024-08-14 17:41:58 -0700

    Time: 81 ms (90.59%) | Memory: 21.2 MB (43.35%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,18 +1,11 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        frequency_map = collections.Counter(nums)
-        heap = []
-        
-        # Push elements into the heap
-        for num, freq in frequency_map.items():
-            if len(heap) < k:
-                heapq.heappush(heap, (freq, num))
-            else:
-                if freq > heap[0][0]:
-                    heapq.heappop(heap)
-                    heapq.heappush(heap, (freq, num))
-        
-        # Extract the top k frequent elements from the heap
-        top_k_frequent = [elem[1] for elem in heap]
-        
-        return top_k_frequent
+        heap=[]
+        count = Counter(nums)
+        for i in count:
+            heapq.heappush(heap,(-count[i],i))
+        ans=[]
+        while k:
+            ans.append(heapq.heappop(heap)[1])
+            k-=1
+        return ans
