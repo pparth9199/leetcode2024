4a122ccc3ba5b4c42ead646585dddedf57d96939
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2024-08-15 12:48:36 -0700

    Time: 38 ms (28.49%) | Memory: 16.4 MB (61.31%) - LeetSync

--- 246-strobogrammatic-number/strobogrammatic-number.py
+++ 246-strobogrammatic-number/strobogrammatic-number.py
@@ -1,15 +1,18 @@
 class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
-        rotated=[]
+        l, r = 0, len(num) - 1
+        mapping = {
+            "0": "0",
+            "1": "1",
+            "8": "8",
+            "6": "9",
+            "9": "6",
+        }
 
-        for c in reversed(num):
-            if c in ['0','1','8']:
-                rotated.append(c)
-            elif c=='6':
-                rotated.append('9')
-            elif c=='9':
-                rotated.append('6')
+        while l <= r:
+            if num[l] in mapping and mapping[num[l]] == num[r]:
+                l += 1
+                r -= 1
             else:
                 return False
-
-        return "".join(rotated) == num
+        return True
