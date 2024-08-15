5502dc1b880bdff85a12250b9a8255247ef6dbb2
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2024-08-15 12:47:37 -0700

    Time: 30 ms (85.70%) | Memory: 16.5 MB (22.74%) - LeetSync

---
+++ 246-strobogrammatic-number/strobogrammatic-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isStrobogrammatic(self, num: str) -> bool:
+        rotated=[]
+
+        for c in reversed(num):
+            if c in ['0','1','8']:
+                rotated.append(c)
+            elif c=='6':
+                rotated.append('9')
+            elif c=='9':
+                rotated.append('6')
+            else:
+                return False
+
+        return "".join(rotated) == num
