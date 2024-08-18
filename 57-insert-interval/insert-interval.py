3f6ce6bdd8607afe37913ad513d4b1da03da6063
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2024-08-18 13:31:52 -0700

    Time: 70 ms (83.60%) | Memory: 19.9 MB (69.16%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,26 +1,22 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        if len(intervals)==0:
-            return [newInterval]
-        for i in range(len(intervals)):
+        n = len(intervals)
+        i = 0
+        res=[]
 
-            i = len(intervals)
-            result = []
-            n = 0
 
-            while n < i and intervals[n][1] < newInterval[0]:
-                result.append(intervals[n])
-                n += 1
+        while i<n and newInterval[0]>intervals[i][1]:
+            res.append(intervals[i])
+            i+=1
 
-            while n < i and intervals[n][0] <= newInterval[1]:
-                newInterval[0] = min(newInterval[0], intervals[n][0])
-                newInterval[1] = max(newInterval[1], intervals[n][1])
-                n += 1
+        while i<n and newInterval[1] >=intervals[i][0]:
+            newInterval[0] = min(newInterval[0],intervals[i][0])
+            newInterval[1] = max(newInterval[1],intervals[i][1])
+            i+=1
+        res.append(newInterval)
 
-            result.append(newInterval)
+        while i<n:
+            res.append(intervals[i])
+            i+=1
 
-            while n < i:
-                result.append(intervals[n])
-                n += 1
-
-            return result           
+        return res
