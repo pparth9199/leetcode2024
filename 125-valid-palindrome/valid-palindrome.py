c6108fee2327d1bdc70b9a7026e9975b09f1edc7
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-02-14 11:59:55 -0500

    Time: 3 ms (98.89%) | Memory: 18 MB (68.41%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,16 +1,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s=s.translate(str.maketrans('', '', string.punctuation))
-        s=s.replace(" ","")
-        s=s.lower()
-        left=0
-        right=len(s)-1
-
-        while left<right:
-            if s[left].lower()!=s[right].lower():
+        s = s.strip()
+        if len(s) == 0 or len(s)==1:
+            return True
+        s = s.replace(" ", "")
+        translator = str.maketrans('', '', string.punctuation)
+        s = s.translate(translator)
+        s = s.lower()
+        p2 = len(s) - 1
+        for p1 in range(0,len(s)//2):
+            if s[p1] != s[p2]:
                 return False
-
-            left+=1
-            right-=1
+            p2 = p2 - 1
         return True
-            
