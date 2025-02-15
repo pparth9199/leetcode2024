d01111bfd80c612eb5722fd879a04cfcd3e19a82
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-02-15 13:51:03 -0500

    Time: 18 ms (7.68%) | Memory: 19.5 MB (19.56%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counter = collections.Counter(nums)
-        currentMax = 0
-        majorityElement = -1
-        for i in counter:
-            if counter[i] > currentMax:
-                currentMax = counter[i]
-                majorityElement = i
-
-        return majorityElement
+        d = dict()
+        for i , j in enumerate(nums):
+            if j not in d:
+                d[j] = 1
+            else:
+                d[j] += 1
+        maximum = 1
+        majority = nums[0]
+        for i in d.keys():
+            if d[i] > maximum:
+                maximum = d[i]
+                majority = i
+        return majority
