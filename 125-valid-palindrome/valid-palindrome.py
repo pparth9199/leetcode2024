fad4a72889e0b807d3c088e6f295a5279a70e43e
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-02-15 16:38:06 -0500

    Time: 3 ms (98.88%) | Memory: 19.7 MB (20.00%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,16 +1,12 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.strip()
-        if len(s) == 0 or len(s) == 1:
-            return True
+        s = s.lower()
         l = []
         for i in s:
             if i.isalnum():
                 l.append(i)
-        news = "".join(l)
-        news = news.lower()
-        print(news)
-        if news[::] == news[::-1]:
+        if l[::] == l[::-1]:
             return True
         else:
             return False
