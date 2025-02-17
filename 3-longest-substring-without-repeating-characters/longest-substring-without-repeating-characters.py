7f85b08e47901cbf5dacab924ef3194c39a7c17b
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-02-16 19:03:30 -0500

    Time: 15 ms (82.53%) | Memory: 17.6 MB (96.76%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -8,11 +8,9 @@ class Solution:
         seen = {}
         i = 0
         for j in range(len(s)):
-            if s[j] not in seen:
-                seen[s[j]] = j + 1
-            else:
+            if s[j] in seen:
                 i = max(seen[s[j]], i)
-                seen[s[j]] = j + 1
+            seen[s[j]] = j + 1
             m = max(m, (j-i)+1)
         return m
 
