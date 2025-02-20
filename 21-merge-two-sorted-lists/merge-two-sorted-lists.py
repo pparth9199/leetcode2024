cf1d858bbed987372759bf9d8f95cc6432389800
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-02-20 18:44:45 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.69%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,20 +5,31 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        merged = ListNode()
-        head = merged
-        
-        while list1 and list2:
-            if list1.val>=list2.val:
-                head.next = list2
-                list2=list2.next
-            elif list2.val>list1.val:
-                head.next = list1
-                list1 = list1.next
-            head=head.next
-        if list1:
-            head.next = list1
-        elif list2:
-            head.next = list2
-            
-        return merged.next
+        p1 = list1
+        p2 = list2
+        head = ListNode()
+        p3 = head
+
+        while (p1 and p2):
+            if p1.val == p2.val:
+                p3.next = ListNode(p1.val)
+                p3=p3.next
+                p3.next= ListNode(p2.val)
+                p3=p3.next
+                p1 = p1.next
+                p2 = p2.next
+            elif p1.val < p2.val:
+                p3.next = ListNode(p1.val)
+                p1 = p1.next
+                p3 = p3.next
+            elif p2.val < p1.val:
+                p3.next = ListNode(p2.val)
+                p2 = p2.next
+                p3 = p3.next
+        if p1:
+            p3.next = p1
+        if p2:
+            p3.next = p2
+        head = head.next
+        return head
+                
