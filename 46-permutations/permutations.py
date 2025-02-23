1f4ac14b40470a7aa4b6bf18062ab992846deb3a
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-02-23 17:59:54 -0500

    Time: 2 ms (40.51%) | Memory: 17.9 MB (80.33%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,14 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def perm(p, up):
-            if len(up) == 0:
-                return [p]  # Return a list containing the current permutation
-            ans = []
-            ch = up[0]
-            for i in range(len(p) + 1):  # Adjusted the loop to allow ch to be inserted at the end
-                first = p[:i]
-                second = p[i:]
-                ans.extend(perm(first + [ch] + second, up[1:]))  # Concatenate lists properly
-            return ans
+        n = len(nums)
+        res = []
+        perms = []
 
-        return perm([], nums)
+        def backtrack():
+            if len(perms) == n:
+                res.append(perms[:])
+                return
+        
+            for num in nums:
+                if num not in perms:
+                    perms.append(num)
+                    backtrack()
+                    perms.pop()
+
+        backtrack()   
+        return res  
