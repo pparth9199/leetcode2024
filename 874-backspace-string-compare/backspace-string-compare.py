8da73b86e89a17bfddd0aaeca469cf997a4708d8
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-02-22 20:41:22 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (14.90%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -1,12 +1,22 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        def refineString(s):
-            stack=[]
-            for i in s:
-                if stack and i=='#':
-                    stack.pop()
-                elif i!='#':
-                    stack.append(i)       
-            return "".join(stack)
-            
-        return refineString(s)==refineString(t)
+        stack1 = []
+        stack2 = []
+        for i in s:
+            if i == "#":
+                if len(stack1) == 0:
+                    continue
+                stack1.pop()
+            else:
+                stack1.append(i)
+        for i in t:
+            if i == "#":
+                if len(stack2) == 0:
+                    continue
+                stack2.pop()
+            else:
+                stack2.append(i)
+        if stack1 == stack2:
+            return True
+        else:
+            return False
