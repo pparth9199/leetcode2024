d0b506d7b13695a402cb978a6900c57242b246db
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-02-24 16:10:23 -0500

    Time: 183 ms (38.91%) | Memory: 18.4 MB (36.43%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,46 +1,19 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        # res = []
-        # if len(s) < len(p):
-        #     return res
-        # l = 0
-        # r = len(p)
-        # anagram = True
-        # while r < len(s)+1:
-        #     ana = s[l:r]
-        #     word = list(p)
-        #     print(ana)
-        #     for i in ana:
-        #         if i not in word:
-        #             anagram = False
-        #             break
-        #         word.remove(i)
-        #     if anagram and not word:
-        #         res.append(l)
-        #     l = l + 1
-        #     r = r + 1
-        #     anagram = True
-        # return res
-        hm, res, pL, sL = defaultdict(int), [], len(p), len(s)
-        if pL > sL: return []
-
-        # build hashmap
-        for ch in p: hm[ch] += 1
-
-        # initial full pass over the window
-        for i in range(pL-1):
-            if s[i] in hm: hm[s[i]] -= 1
+        p_dict = collections.Counter(p)
+        s_dict = collections.Counter(s[:len(p)-1]) 
+        l = 0
+        # final result list
+        res = []
+        # We iterate over the string s, and in each step we check if s_dict and p_dict match
+        for r in range(len(p)-1, len(s)):
+            # updating the counter & adding the character
+            s_dict[s[r]] += 1
+            # checking if counters match
+            if s_dict == p_dict:
+                res.append(l)
+            # remove the first element from counter
+            s_dict[s[l]] -= 1
+            l += 1
             
-        # slide the window with stride 1
-        for i in range(-1, sL-pL+1):
-            if i > -1 and s[i] in hm:
-                hm[s[i]] += 1
-            if i+pL < sL and s[i+pL] in hm: 
-                hm[s[i+pL]] -= 1
-                
-            # check whether we encountered an anagram
-            if all(v == 0 for v in hm.values()): 
-                res.append(i+1)
-                
         return res
-            
