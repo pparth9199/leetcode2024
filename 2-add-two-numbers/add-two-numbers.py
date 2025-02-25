cae65feb730051fbe152e00d15ce2260339a2324
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-02-25 13:46:45 -0500

    Time: 9 ms (14.50%) | Memory: 17.9 MB (57.17%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,22 +5,43 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        summ=ListNode()
-        res = summ
-        carry=0
-        while l1 or l2 or carry:
-            one = l1.val if l1 else 0
-            two = l2.val if l2 else 0
-            nodeSum = one+two+carry
-            carry=nodeSum//10
-            nodeSum=nodeSum%10
-            temp = ListNode(nodeSum)
-            summ.next = temp
-            summ=summ.next
-            if l1:
-                l1=l1.next
-            if l2:
-                l2=l2.next
-            
-        return(res.next)
-            
+        num1 = []
+        num2 = []
+        
+        p1 = l1
+        while p1:
+            num1.append(p1.val)
+            p1 = p1.next
+        
+        p2 = l2
+        while p2:
+            num2.append(p2.val)
+            p2 = p2.next
+        num1[::] = num1[::-1]
+        num2[::] = num2[::-1]
+
+        n1 = 0
+        n2 = 0
+       
+        for i in range(len(num1)):
+            zero = len(num1) - i - 1
+            n1 = n1 + (num1[i] * (10**zero))
+       
+        for i in range(len(num2)):
+            zero = len(num2) - i - 1
+            n2 = n2 + (num2[i] * (10**zero))
+
+        s = n1 + n2
+        
+        l = list(map(int, str(s)))
+        l[::] = l[::-1]
+        
+        head = ListNode()
+        p = head
+        
+        for i in range(len(l)-1):
+            p.val = l[i]
+            p.next = ListNode()
+            p = p.next
+        p.val = l[-1]
+        return head
