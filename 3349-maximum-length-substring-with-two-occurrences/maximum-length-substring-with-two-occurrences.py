c15e100a684b743c69f4d30481f105caafcbd176
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-02-27 01:02:29 -0500

    Time: 7 ms (45.19%) | Memory: 17.7 MB (54.31%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
@@ -1,32 +1,15 @@
 class Solution:
     def maximumLengthSubstring(self, s: str) -> int:
-        # visited = defaultdict(int)
-        # l = 0
-        # r = 0
-        # ans = 0
-        # while l < len(s) and r < len(s):
-        #     if s[r] not in visited or visited[s[r]] < 2:
-        #         ans = max(ans, r - l + 1)
-        #         visited[s[r]] += 1
-        #         r = r + 1
-        #     else:
-        #         l = l + 1
-        #         visited[s[l]] -= 1
-        # return ans
-        if len(s) > 2 and len(set(s)) == 1:
-            return 2
-
-        d = defaultdict(int)
+        visited = defaultdict(int)
         l = 0
         r = 0
-        res = 0
-
+        ans = 0
         while l < len(s) and r < len(s):
-            if s[r] not in d or d[s[r]] < 2:
-                d[s[r]] +=1
-                res = max(res, r - l + 1)
-                r+=1
+            if s[r] not in visited or visited[s[r]] < 2:
+                ans = max(ans, r - l + 1)
+                visited[s[r]] += 1
+                r = r + 1
             else:
-                d[s[l]]-=1
-                l+=1
-        return res
+                visited[s[l]] -= 1
+                l = l + 1
+        return ans
