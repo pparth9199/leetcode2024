dadeb148e2a71e417bd5c0a41fe0d45fe04b4686
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-02-28 01:15:04 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (61.04%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -6,36 +6,23 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        # res = []
-        # if not root:
-        #     return res
-        # queue = deque([root])
-        # while queue:
-        #     currlevel = []
-        #     for _ in range(len(queue)):
-        #         node = queue.popleft()
-        #         currlevel.append(node.val)
-        #         if node.left:
-        #             queue.append(node.left)
-        #         if node.right:
-        #             queue.append(node.right)
-        #     res.append(currlevel)
-        # ans = []
-        # for i in range(len(res)):
-        #     ans.append(res[i][-1])
-        # return ans
-        ans = []
         if not root:
-            return ans
+            return []
         queue = deque([root])
+        levels = []
         while queue:
-            size = len(queue)
-            for i in range(size):
+            curr_level = []
+            n = len(queue)
+            for i in range(n):
                 node = queue.popleft()
-                if i == size-1:
-                    ans.append(node.val)
+                curr_level.append(node.val)
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
+            levels.append(curr_level)
+        ans = []
+        for i in range(len(levels)):
+            ans.append(levels[i][-1])
         return ans
+            
