f6dded6bf7039d3663d819efc9ca5512f1ea2da9
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-03-01 02:45:37 -0500

    Time: 0 ms (100.00%) | Memory: 19.2 MB (63.20%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,30 +1,21 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         pre = defaultdict(list)
-
-        for course, p in prerequisites:
-            pre[course].append(p)
-        
+        for i in range(len(prerequisites)):
+            pre[prerequisites[i][0]].append(prerequisites[i][1])
         visited = set()
-
         def dfs(course):
             if not pre[course]:
                 return True
-            
             if course in visited:
                 return False
-            
             visited.add(course)
-
             for p in pre[course]:
                 if not dfs(p):
                     return False
-            
             pre[course] = []
             return True
-        
         for c in range(numCourses):
             if not dfs(c):
                 return False
-        
         return True
