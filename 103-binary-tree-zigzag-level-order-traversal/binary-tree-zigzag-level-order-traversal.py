071fe96a201f542393f45ad1b4a998bd694ec557
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-03-01 02:04:45 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (77.94%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -6,29 +6,23 @@
 #         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res=[]
+        levels = []
         if not root:
-            return res
-
-        q=[]
-        q.append(root)
-        flag=False
-        while q:
-            level = []
-            size = len(q)
-            for i in range(size):
-                thisNode = q.pop(0)
-                
-                
-                
-                if thisNode.left:
-                    q.append(thisNode.left)
-                if thisNode.right:
-                    q.append(thisNode.right)
-                if flag:
-                    level.insert(0,thisNode.val)
-                else:
-                    level.append(thisNode.val)
-            flag=not flag        
-            res.append(level)
-        return res
+            return levels
+        queue = deque([root])
+        zig = 0
+        while queue:
+            curr_level = []
+            n = len(queue)
+            for i in range(n):
+                node = queue.popleft()
+                curr_level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            if zig % 2 != 0:
+                curr_level[:] = curr_level[::-1]
+            zig += 1
+            levels.append(curr_level)
+        return levels
