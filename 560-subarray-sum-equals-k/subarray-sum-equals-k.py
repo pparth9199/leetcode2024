99b9d08d48a58dac8e5c1975df5df3d3704b6f6f
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-03-02 16:06:21 -0500

    Time: 35 ms (46.61%) | Memory: 20.4 MB (70.08%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,21 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        prefix_sum = 0
+        prefix_map = defaultdict(int)
+        prefix_map[0] = 1
+        count = 0 
 
-        count = 0
-        curr_sum = 0
-        sum_map = {0: 1}  # Initialize the sum map with sum 0 having one count
-        
         for num in nums:
-            curr_sum += num
-            
-            # Check if (curr_sum - k) exists in sum_map
-            if (curr_sum - k) in sum_map:
-                count += sum_map[curr_sum - k]
-            
-            # Update the sum_map with the current cumulative sum
-            if curr_sum in sum_map:
-                sum_map[curr_sum] += 1
-            else:
-                sum_map[curr_sum] = 1
-        
+            prefix_sum += num
+            if prefix_sum - k in prefix_map:
+                count += prefix_map[prefix_sum - k]
+            prefix_map[prefix_sum] += 1
         return count
