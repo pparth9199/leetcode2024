8410eb0553813e1b8aeeb5add0f03e54166ff22e
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-03-05 17:01:32 -0500

    Time: 1 ms (93.42%) | Memory: 17.8 MB (64.16%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,17 +1,5 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        # reversing a string
-        return str(x)==str(x)[::-1]
-
-        # Without str conversion
         if x<0:
             return False
-        if x==0:
-            return True
-        def reverse(num,n):
-            if n==0:
-                return num
-            current = num%10
-            remain = num//10
-            return (current * 10**(n)) + reverse(remain,n-1)
-        return x==reverse(x,len(str(x))-1)
+        return str(x)==str(x)[::-1]
