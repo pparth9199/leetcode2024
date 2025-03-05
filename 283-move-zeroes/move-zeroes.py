656822e40cc1ce369684e4c39af68908bd7c2bb5
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-03-05 15:33:41 -0500

    Time: 4 ms (64.62%) | Memory: 18.6 MB (94.51%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,17 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        c = 0
-        non = []
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                c += 1
-            else:
-                non.append(nums[i])
-        i = 0
-        for j in range(len(non)):
-            nums[i] = non[j]
-            i += 1
-        for j in range(c):
-            nums[i] = 0     
-            i += 1   
+        left = 0
+
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[right], nums[left] = nums[left], nums[right]
+                left += 1
+        
+        return nums
