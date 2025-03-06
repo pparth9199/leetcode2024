71da94fa8ce49485972c5232d3f6392f95aa037a
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-03-06 18:11:39 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (33.45%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,12 +3,4 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        sort = False
-        while not sort:
-            sort = True
-            for i in range(0, len(nums)-1):
-                if nums[i] > nums[i+1]:
-                    nums[i], nums[i+1] = nums[i+1], nums[i]
-                    sort = False
-        
-        
+        nums.sort()
