62ef80871894b2ba288fe4cbbd54d1ca5a4b39a3
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-03-06 18:36:52 -0500

    Time: 3 ms (42.91%) | Memory: 17.8 MB (75.42%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,24 +1,14 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        if len(s) == 1:
-            return 1
-        if len(set(s)) == 1:
-            return len(s)
-        d = {}
-        l = 0
-        odd = 1
-        for i in set(s):
-            d[i] = s.count(i)
-        for i in d:
-            if d[i] % 2 == 0 :
-                l = l + d[i]
-            elif d[i] == 1 and odd == 1:
-                l = l + d[i]
-                odd = odd + 1
-            elif d[i] % 2 != 0 and d[i] > 2:
-                if odd == 1:
-                    l = l + d[i]
-                    odd = odd + 1
-                else:
-                    l = l + d[i] - 1
-        return l
+        character_set = set()
+        res = 0
+
+        for c in s:
+            if c in character_set:
+                character_set.remove(c)
+                res+=2
+            else:
+                character_set.add(c)
+        if character_set:
+            res+=1
+        return res
