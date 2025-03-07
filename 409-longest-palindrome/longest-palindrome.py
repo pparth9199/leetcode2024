48232cdb3cf7430e1c065f354c4b7dbd1d0600c1
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-03-06 20:19:19 -0500

    Time: 4 ms (14.38%) | Memory: 17.8 MB (75.42%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -2,7 +2,6 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         character_set = set()
         res = 0
-
         for c in s:
             if c in character_set:
                 character_set.remove(c)
