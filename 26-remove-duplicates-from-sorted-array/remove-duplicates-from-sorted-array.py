d351a4b34a279011efb975c00c4df67f9c3c9f28
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-03-13 13:21:53 -0400

    Time: 21 ms (12.46%) | Memory: 18.9 MB (75.46%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,19 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        
-        # Initialize two pointers
-        left = 1  # This will point to the position where the next unique element should be placed
-        
-        # Iterate through the array starting from the second element
-        for right in range(1, len(nums)):
-            # If the current element is not equal to the previous one, it is unique
-            if nums[right] != nums[right - 1]:
-                # Place the unique element at the left pointer's position
-                nums[left] = nums[right]
-                left += 1  # Move the left pointer to the next position
-                
-        # The value of `left` will be the count of unique elements
-        return left
+        seen = []
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                nums[i]=inf
+            else:
+                seen.append(nums[i])
+        nums = nums.sort()
+        return(len(seen))
         
