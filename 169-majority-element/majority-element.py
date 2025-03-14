97de5140c9864e693fc7736a5fd071956613f566
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-03-14 13:54:03 -0400

    Time: 3 ms (82.27%) | Memory: 19.4 MB (52.13%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hash_map = Counter(nums)
-        for i in hash_map:
-            if hash_map[i]>(len(nums)//2):
-                return i
-        
+        n = len(nums)//2
+        cache = Counter(nums)
+
+        for k,v in cache.items():
+            if v>n:
+                return k
+
+            
