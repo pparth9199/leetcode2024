61c5036d1062b8dac20eee0ca494bb6a1835dd02
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-03-14 13:41:33 -0400

    Time: 3 ms (48.49%) | Memory: 19.1 MB (9.31%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,11 +4,11 @@ class Solution:
         r=1
 
         while r<len(nums):
-            if nums[r]==nums[r-1]:
-                r+=1
-            else:
+            if nums[r]!=nums[r-1]:
                 nums[l]=nums[r]
                 l+=1
+                r+=1
+            else:
                 r+=1
         return l
         
