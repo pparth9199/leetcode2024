2f2b4110864a249e71d98cfb5a64c10624f5db7b
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-04 13:49:40 -0400

    Time: 36 ms (43.50%) | Memory: 20.3 MB (70.75%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,13 +1,14 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
         prefix_sum = 0
-        prefix_map = defaultdict(int)
-        prefix_map[0] = 1
-        count = 0 
-
+        prefix_sum_count = defaultdict(int)
+        prefix_sum_count[0] = 1  # Initialize with prefix sum 0 and count 1
+        
         for num in nums:
-            prefix_sum += num
-            if prefix_sum - k in prefix_map:
-                count += prefix_map[prefix_sum - k]
-            prefix_map[prefix_sum] += 1
+            prefix_sum += num  # Update the running prefix sum
+            if (prefix_sum - k) in prefix_sum_count:
+                count += prefix_sum_count[prefix_sum - k]  # Increment count if (prefix_sum - k) is found
+            prefix_sum_count[prefix_sum] += 1  # Update the frequency if the current prefix_sum
+        
         return count
