32c70af0a4819dca1f6fcea68302e515a966de3a
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-03 20:20:02 -0400

    Time: 3 ms (90.97%) | Memory: 21.2 MB (80.54%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,13 +1,15 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # O(1) time 
         if k == len(nums):
             return nums
+
+        countDict = Counter(nums)
+    
+        heap = []
+        for num, count in countDict.items():
+            heapq.heappush(heap, [-count, num])
         
-        # 1. Build hash map: character and how often it appears
-        # O(N) time
-        count = Counter(nums)   
-        # 2-3. Build heap of top k frequent elements and
-        # convert it into an output array
-        # O(N log k) time
-        return heapq.nlargest(k, count.keys(), key=count.get) 
+        ans = []
+        for i in range(k):
+            ans.append(heapq.heappop(heap)[1])
+        return ans
