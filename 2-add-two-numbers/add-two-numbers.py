49a4f836519cff60c227d917488998201b63eb0c
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-05 18:22:18 -0400

    Time: 10 ms (12.29%) | Memory: 17.8 MB (57.62%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,49 +5,19 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        if not l1 and not l2:
-            return None
-        if not l1:
-            return l2
-        if not l2:
-            return l1
-        num1 = []
-        num2 = []
-        
-        p1 = l1
-        while p1:
-            num1.append(p1.val)
-            p1 = p1.next
-        
-        p2 = l2
-        while p2:
-            num2.append(p2.val)
-            p2 = p2.next
-        num1[::] = num1[::-1]
-        num2[::] = num2[::-1]
-
-        n1 = 0
-        n2 = 0
-       
-        for i in range(len(num1)):
-            zero = len(num1) - i - 1
-            n1 = n1 + (num1[i] * (10**zero))
-       
-        for i in range(len(num2)):
-            zero = len(num2) - i - 1
-            n2 = n2 + (num2[i] * (10**zero))
-
-        s = n1 + n2
-        
-        l = list(map(int, str(s)))
-        l[::] = l[::-1]
-        
-        head = ListNode()
-        p = head
-        
-        for i in range(len(l)-1):
-            p.val = l[i]
-            p.next = ListNode()
-            p = p.next
-        p.val = l[-1]
+        s1=s2=""
+        while l1 or l2:
+            if l1:
+                s1+=str(l1.val)
+                l1=l1.next
+            if l2:
+                s2+=str(l2.val)
+                l2 = l2.next
+        summ = str(int(s1[::-1])+int(s2[::-1]))
+        listSum = list(summ)
+        head = ListNode(int(listSum[-1]))
+        iterator = head
+        for i in range(len(listSum)-2,-1,-1):
+            iterator.next = ListNode(int(listSum[i]))
+            iterator = iterator.next
         return head
