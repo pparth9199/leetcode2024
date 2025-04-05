38a0138fe3a422cc1110448c49a6061304f2cec1
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-05 16:54:27 -0400

    Time: 0 ms (100.00%) | Memory: 21.2 MB (9.08%) - LeetSync

--- 346-moving-average-from-data-stream/moving-average-from-data-stream.py
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.py
@@ -2,11 +2,21 @@ class MovingAverage:
 
     def __init__(self, size: int):
         self.size = size
-        self.storage = []
+        self.storage = deque()
+        self.current_sum = 0
 
     def next(self, val: int) -> float:
         self.storage.append(val)
+        self.current_sum += val
         current_size = len(self.storage)
+        if current_size > self.size:
+            removed_val = self.storage.popleft()
+            self.current_sum -= removed_val
+            return self.current_sum/self.size
+        else:
+            return self.current_sum/current_size
+
+        
         if current_size < self.size:
             return sum(self.storage)/current_size
         else:
