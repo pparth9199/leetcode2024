fefbdbcbb02de9c48eb3726912dd1c17fa676e9a
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-05 13:30:07 -0400

    Time: 54 ms (82.94%) | Memory: 21.9 MB (24.05%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,40 +1,31 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        # Stores the closest number encountered. 
-        # Therefore, is the right operand.
-        num = 0
-        # previous operator that we have encountered.
-        pre_op = '+'
-        # To allow processing the last pre_op in the stack.
-        # In fact, any one of +-*/ works.
-        s += '+'
+        def evaluate(operator, x, y = 0):
+            if operator == "+":
+                return x
+            if operator == "-":
+                return -x
+            if operator == "*":
+                return x * y
+            return int(x / y)
+        
         stack = []
+        curr = 0
+        previous_operator = "+"
+        s += "@"
+        
         for c in s:
-            # Handle multi-digit numbers.
-            if c.isdigit():
-                num = num * 10 + int(c)
-            # Ignore whitespaces.
-            elif c == ' ':
+            if c == " ":
                 continue
-            # c is an operator in +-*/
+            if c.isdigit():
+                curr = curr * 10 + int(c)
             else:
-                if pre_op == '+':
-                    stack.append(num)
-                elif pre_op == '-':
-                    stack.append(-num)
-                # * and / has priority over + and -.
-                # So directly calculate their result and add to stack.
-                elif pre_op == '*':
-                    operand = stack.pop()
-                    # Push evaluated value to stack.
-                    stack.append(operand * num)
-                elif pre_op == '/':
-                    operand = stack.pop()
-                    # Round division result to integer closest to zero.
-                    stack.append(math.trunc(operand / num))
-                # reset operand number to 0
-                num = 0
-                # Update operator that we have encountered that we should work with.
-                pre_op = c
-        # stack is left with integers to add together.
+                if previous_operator in "*/":
+                    stack.append(evaluate(previous_operator, stack.pop(), curr))
+                else:
+                    stack.append(evaluate(previous_operator, curr))
+                
+                curr = 0
+                previous_operator = c
+
         return sum(stack)
