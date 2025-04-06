899aa5735d572655a6df50dd1a636c37525e9853
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-06 12:22:50 -0400

    Time: 3 ms (44.54%) | Memory: 19.1 MB (17.97%) - LeetSync

---
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        if not arr: return 0
+        if len(set(arr)) == 1:
+            return arr[0]
+
+
+        cache = defaultdict(int)
+        quarter = len(arr)/4
+        for i in arr:
+            cache[i] += 1
+            if cache[i] > quarter:
+                return i
+        
