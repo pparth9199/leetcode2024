a4a61481977d53bd551392dad6e265ec9a09f94e
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-06 19:28:28 -0400

    Time: 235 ms (84.14%) | Memory: 20.2 MB (58.72%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,30 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        count = 0
-        if not grid:
-            return count
-        
-        m = len(grid)
-        n = len(grid[0])
+        rows = len(grid)
+        cols = len(grid[0])
 
-        def dfs(r,c):
+
+        def bfs(r,c):
+            queue = deque([(r, c)])
             grid[r][c] = "2"
-            if r > 0:
-                if grid[r-1][c] == "1":
-                    dfs(r-1,c)
-            if r < m-1:
-                if grid[r+1][c] == "1":
-                    dfs(r+1,c)
-            if c > 0:
-                if grid[r][c-1] == "1":
-                    dfs(r,c-1)
-            if c < n-1:
-                if grid[r][c+1] == "1":
-                    dfs(r, c+1)
-            
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1":
-                    count = count + 1
-                    dfs(i,j)
+
+            while queue:
+                dr,dc = queue.popleft()
+                
+                for row,col in [(-1,0),(1,0),(0,-1),(0,1)]:
+                    nr,nc = dr+row,dc+col
+                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]=="1":
+                        grid[nr][nc]="2"
+                        queue.append([nr,nc])
+
+        count = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=="1":
+                    count+=1
+                    bfs(r,c)
         return count
