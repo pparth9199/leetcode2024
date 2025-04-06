6d240b8c8c3a40faccd434ebed0f5bb058c2b216
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-06 19:36:48 -0400

    Time: 239 ms (76.66%) | Memory: 20 MB (95.55%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -7,6 +7,7 @@ class Solution:
         def bfs(r,c):
             queue = deque([(r, c)])
             grid[r][c] = "2"
+            size = 1
 
             while queue:
                 dr,dc = queue.popleft()
@@ -16,11 +17,26 @@ class Solution:
                     if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]=="1":
                         grid[nr][nc]="2"
                         queue.append([nr,nc])
+                        size+=1
+            return size 
+
+        def dfs(r,c):
+            if r<0 or r>=rows or c<0 or c>=cols or grid[r][c]!="1":
+                return 0
+            
+            grid[r][c] = "2"
+            size = 1
+
+            for dr,dc in [(-1,0),(1,0),(0,1),(0,-1)]:
+                size+=dfs(r+dr,c+dc)
+            return size
 
         count = 0
+        sizes = []
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c]=="1":
                     count+=1
-                    bfs(r,c)
+                    # sizes.append(bfs(r,c))
+                    sizes.append(dfs(r,c))
         return count
