673f437799a90e887fe612bf72c2c305436f04eb
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-07 16:58:58 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (40.32%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -3,7 +3,7 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
 
-        def backtrack(p: List[int], up: List[int]):
+        def backtrack(p, up):
             res.append(p[:])  # Add the current subset
 
             for i in range(len(up)):
