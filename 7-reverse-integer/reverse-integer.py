d681ae8430f635ece10c1f6c628a300cba03e0b6
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-07 00:46:27 -0400

    Time: 37 ms (70.43%) | Memory: 17.6 MB (97.14%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,14 +1,19 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x>=(2**31)-1 or x<=-2**31:
+        if x<0:
+            sign = -1
+        else:
+            sign = 1
+        
+        res,x = 0, abs(x)
+
+
+        while x:
+            mod = x%10
+            x=x//10
+            res = res*10 + mod
+
+        if ((res > (2**31 - 1)) or (res < -2**31)):
             return 0
-        def helper(num,n):
-            if n==0:
-                return num
-            current = num%10
-            remain = num//10
-            return (current * 10**(n)) + helper(remain,n-1)
-        reversedNum = helper(abs(x),len(str(abs(x)))-1) * (-1 if x<0 else 1)
-        if reversedNum>=(2**31)-1 or reversedNum<=-2**31:
-            return 0
-        return reversedNum
+        
+        return sign*res
