d6dad0da486e7eb78a4391a1f5c5993d644fb497
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-08 16:27:44 -0400

    Time: 43 ms (22.14%) | Memory: 20.4 MB (37.16%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -6,9 +6,9 @@ class Solution:
         prefix_sum_count[0] = 1  # Initialize with prefix sum 0 and count 1
         
         for num in nums:
-            prefix_sum += num  # Update the running prefix sum
+            prefix_sum += num 
             if (prefix_sum - k) in prefix_sum_count:
-                count += prefix_sum_count[prefix_sum - k]  # Increment count if (prefix_sum - k) is found
+                count += prefix_sum_count[prefix_sum - k]
             prefix_sum_count[prefix_sum] += 1  # Update the frequency if the current prefix_sum
         
         return count
