df0bc088438725285bbf8cd18c1c60738918ad59
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-07 20:10:43 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (13.09%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -1,20 +1,15 @@
 class Solution:
     def removeNthFromEnd(self, head, n):
-        """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
-        """
         dummy = ListNode(0)
         dummy.next = head
-        first = dummy
-        second = dummy
-        # Advances first pointer so that the gap between first and second is n nodes apart
+        fast = dummy
+        slow = dummy
+ 
         for i in range(n + 1):
-            first = first.next
-        # Move first to the end, maintaining the gap
-        while first is not None:
-            first = first.next
-            second = second.next
-        second.next = second.next.next
+            fast = fast.next
+
+        while fast is not None:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
         return dummy.next
