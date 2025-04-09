cebff42bc68c1264b2e32506948bce4bcfb755a3
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-09 17:19:31 -0400

    Time: 0 ms (100.00%) | Memory: 18.4 MB (25.47%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,30 +1,49 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-       
-        def rowSearch(nums):
-            l,r=0,len(nums)-1
-            print(nums)
-            while l<=r:
-                mid = (l+r)//2
-                print(mid)
-                if nums[mid]==target:
-                    return True
-                elif nums[mid]<target:
-                    l=mid+1
-                else:
-                    r=mid-1
-
+        if not matrix or not matrix[0]:
             return False
 
-        l,r=0,len(matrix)-1
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
 
-        while l<=r:
-            mid=(l+r)//2
+        while left <= right:
+            mid = (left + right) // 2
+            row = mid // n
+            col = mid % n
+            mid_val = matrix[row][col]
 
-            if matrix[mid][0]>target:
-                r=mid-1
-            elif matrix[mid][0]<=target and matrix[mid][-1]>=target:
-                return rowSearch(matrix[mid])
+            if mid_val == target:
+                return True
+            elif mid_val < target:
+                left = mid + 1
             else:
-                l=mid+1
+                right = mid - 1
+
         return False
+        # def rowSearch(nums):
+        #     l,r=0,len(nums)-1
+        #     print(nums)
+        #     while l<=r:
+        #         mid = (l+r)//2
+        #         print(mid)
+        #         if nums[mid]==target:
+        #             return True
+        #         elif nums[mid]<target:
+        #             l=mid+1
+        #         else:
+        #             r=mid-1
+
+        #     return False
+
+        # l,r=0,len(matrix)-1
+
+        # while l<=r:
+        #     mid=(l+r)//2
+
+        #     if matrix[mid][0]>target:
+        #         r=mid-1
+        #     elif matrix[mid][0]<=target and matrix[mid][-1]>=target:
+        #         return rowSearch(matrix[mid])
+        #     else:
+        #         l=mid+1
+        # return False
