415f48b59e76ce9f7b2f08313f5fd2e8e0a8edb3
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-09 19:37:54 -0400

    Time: 0 ms (100.00%) | Memory: 22.4 MB (87.40%) - LeetSync

--- 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.py
+++ 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.py
@@ -1,10 +1,11 @@
 class Solution:
     def missingElement(self, nums: List[int], k: int) -> int:
         l,r=0,len(nums)-1
-        diff = nums[-1] - nums[0] + 1 # complete length
-        missing = diff - len(nums) # complete length - real length
-        if k > missing: # if k is larger than the number of mssing words in sequence
-            return nums[-1] + k - missing
+        expected_len = nums[-1] - nums[0] + 1 
+        no_of_missing = expected_len - len(nums) 
+        if k > no_of_missing: 
+            return nums[-1] + k - no_of_missing
+            
         while l+1<r:
             mid = (l+r)//2
             missing = nums[mid] - nums[l] - mid +l
