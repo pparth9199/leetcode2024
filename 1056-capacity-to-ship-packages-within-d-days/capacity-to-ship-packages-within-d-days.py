98927b25a68b8896885bdce04f0d88208a99d6d3
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-09 19:04:29 -0400

    Time: 38 ms (99.78%) | Memory: 22.2 MB (55.49%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,20 +1,39 @@
+# class Solution:
+#     def shipWithinDays(self, weights: List[int], days: int) -> int:
+#         left, right = max(weights), sum(weights)
+#         while left < right:
+#             mid = (left + right) // 2
+#             need=1
+#             cur=0
+
+#             for w in weights:
+#                 if cur + w > mid:
+#                     need += 1
+#                     cur = 0
+#                 cur += w
+
+#             if need > days: 
+#                 left = mid + 1
+#             else: 
+#                 right = mid
+
+#         return left
 class Solution:
-    def shipWithinDays(self, weights: List[int], days: int) -> int:
-        left, right = max(weights), sum(weights)
-        while left < right:
-            mid = (left + right) // 2
-            need=1
-            cur=0
-
-            for w in weights:
-                if cur + w > mid:
-                    need += 1
-                    cur = 0
-                cur += w
-
-            if need > days: 
-                left = mid + 1
-            else: 
-                right = mid
-
-        return left
+    def shipWithinDays(self, W: List[int], days: int) -> int:
+        maxW= max(W) 
+        l = max(ceil(sum(W)/days), maxW)
+        r = min(l + maxW, ceil(len(W) / days) * maxW) # r = l + maxW # also works almost the same. 
+        while l < r:
+            mid = (l+r)//2 
+            curTotal = 0
+            daysNeeded = 1 #make sure to initialize to 1 as we are only incrementing daysNeeded when the current load exceeds capacity
+            for weight in W:           
+                curTotal += weight 
+                if curTotal > mid:    #ship current load- wait a day to ship more 
+                    curTotal = weight
+                    daysNeeded += 1
+            if daysNeeded <= days: #capacity is sufficient- see if smaller capacities work
+                r = mid
+            else: #capacity is too high- reduce search space to higher capacities
+                l = mid + 1
+        return r
