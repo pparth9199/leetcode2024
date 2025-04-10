7854db7f17d167abf4de180258c538ae60a1037a
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-10 13:23:16 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (9.61%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,19 +1,25 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        cachedAdder, digit = 1,len(digits)-1
-        
-        while cachedAdder:
-            print(digits[digit])
-            if digit>=0:
-                if digits[digit] == 9:
-                    digits[digit]=0
-                else:
-                    digits[digit]+=1
-                    cachedAdder=0
-            else:
-                digits.insert(0,1)
-                cachedAdder=0
-            digit-=1
-        
+        carry = 0
+        n=digits[-1]+1
+        num = n%10
+        carry=n//10
+        digits[-1]=num
+        if carry==0:
+            return digits
+        else:
+            for i in range(len(digits)-2,-1,-1):
+                n=digits[i]+carry
+                num = n%10
+                carry=n//10
+                digits[i]=num
+                if carry==0:
+                    return digits
+
+            if carry!=0:
+                digits.insert(0,carry)
+
         return digits
-                
+
+        
+            
