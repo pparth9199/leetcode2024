b147a010cd82bed3bf2219a2d47cd5c1163977b1
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-11 18:47:43 -0400

    Time: 139 ms (75.45%) | Memory: 63.1 MB (40.97%) - LeetSync

--- 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
+++ 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxSubArrayLen(self, nums: List[int], k: int) -> int:
         prefix_sum = 0
-        prefix_map = {0: -1}  # sum : first_index
+        prefix_map = {0: -1}  
         max_len = 0
 
         for i, num in enumerate(nums):
@@ -11,6 +11,6 @@ class Solution:
                 max_len = max(max_len, i - prefix_map[prefix_sum - k])
 
             if prefix_sum not in prefix_map:
-                prefix_map[prefix_sum] = i  # store first occurrence only
+                prefix_map[prefix_sum] = i 
 
         return max_len
