bca3f6234572f54418c6c0cae968b24e4478fde8
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-11 21:06:25 -0400

    Time: 3 ms (28.08%) | Memory: 17.7 MB (78.86%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -4,18 +4,25 @@ class Solution:
             return ""
 
         prefix = strs[0]
-        for word in strs[1:]:
-            while not word.startswith(prefix):
-                prefix = prefix[:-1]
-                if not prefix:
-                    return ""
+
+        for i in range(1, len(strs)):
+            word = strs[i]
+            j = 0
+            while j < len(prefix) and j < len(word) and prefix[j] == word[j]:
+                j += 1
+            prefix = prefix[:j]
+            if prefix == "":
+                break
 
         return prefix
+        # if not strs:
+        #     return ""
 
-    # prefix = strs[0]
-    # for word in strs[1:]:
-    #     while not word.startswith(prefix):
-    #         prefix = prefix[:-1]
-    #         if not prefix:
-    #             return ""
-    # return prefix
+        # prefix = strs[0]
+        # for word in strs[1:]:
+        #     while not word.startswith(prefix):
+        #         prefix = prefix[:-1]
+        #         if not prefix:
+        #             return ""
+
+        # return prefix
