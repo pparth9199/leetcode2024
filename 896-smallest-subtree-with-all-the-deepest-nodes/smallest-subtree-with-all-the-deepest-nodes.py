ad035cc466cd3b95ea9825463280ffc804e55df4
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-12 21:03:59 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (80.51%) - LeetSync

---
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        dist = 0
+        max_dist = -1
+        res=None
+        def dfs(root,dist):
+            nonlocal max_dist,res
+            if not root:
+                max_dist = max(max_dist,dist)
+                return dist
+            
+            left = dfs(root.left,dist+1)
+
+            right = dfs(root.right,dist+1)
+            if left==right and left == max_dist:
+                res = root
+            return max(left,right) 
+
+        dfs(root,0)
+        return res
