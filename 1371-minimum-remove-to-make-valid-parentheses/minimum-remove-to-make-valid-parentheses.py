73a288a542a660a22f057441ff5d9b40d06b29b8
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-13 21:28:03 -0400

    Time: 43 ms (60.98%) | Memory: 21.2 MB (5.92%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,18 +1,52 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        if not s:
-            return True
-        stack = []
-        sl = list(s)
-        for i in range(len(sl)):
-            if sl[i] == "(":
-                stack.append(i)
-            elif sl[i] == ")":
-                if stack:
-                    stack.pop()
-                else:
-                    sl[i] = ""
-        for i in stack:
-            sl[i] = ""
-        return "".join(sl)
+        # First pass: remove unmatched ')'
+        result = []
+        open_count = 0
+        for c in s:
+            if c == '(':
+                open_count += 1
+                result.append(c)
+            elif c == ')':
+                if open_count > 0:
+                    open_count -= 1
+                    result.append(c)
+                # else skip it (unmatched ')')
+            else:
+                result.append(c)
+
+        # Second pass: remove unmatched '(' from the end
+        final = []
+        open_count = 0
+        for c in reversed(result):
+            if c == ')':
+                open_count += 1
+                final.append(c)
+            elif c == '(':
+                if open_count > 0:
+                    open_count -= 1
+                    final.append(c)
+                # else skip it (unmatched '(')
+            else:
+                final.append(c)
+
+        return ''.join(reversed(final))
+
+# class Solution:
+#     def minRemoveToMakeValid(self, s: str) -> str:
+#         if not s:
+#             return True
+#         stack = []
+#         sl = list(s)
+#         for i in range(len(sl)):
+#             if sl[i] == "(":
+#                 stack.append(i)
+#             elif sl[i] == ")":
+#                 if stack:
+#                     stack.pop()
+#                 else:
+#                     sl[i] = ""
+#         for i in stack:
+#             sl[i] = ""
+#         return "".join(sl)
 
