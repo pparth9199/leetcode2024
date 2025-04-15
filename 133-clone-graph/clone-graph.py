c8016877ee434eb78c25388ce731bb02431f253b
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-04-15 19:59:31 -0400

    Time: 45 ms (46.67%) | Memory: 18.3 MB (16.74%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -1,27 +1,17 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
-
-from typing import Optional
 class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        oldToNew = {}
-
-        def dfs(node):
-            if node in oldToNew:
-                return oldToNew[node]
-
-            copy = Node(node.val)
-            oldToNew[node] = copy
-            for n in node.neighbors:
-                copy.neighbors.append(dfs(n))
-            return copy
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node: return node
         
-        return dfs(node) if node else None
+        q, clones = deque([node]), {node.val: Node(node.val, [])}
+        while q:
+            cur = q.popleft() 
+            cur_clone = clones[cur.val]            
 
-
-            
+            for ngbr in cur.neighbors:
+                if ngbr.val not in clones:
+                    clones[ngbr.val] = Node(ngbr.val, [])
+                    q.append(ngbr)
+                    
+                cur_clone.neighbors.append(clones[ngbr.val])
+                
+        return clones[node.val]
