ddb208cef776445b1ccbf9551ea08fe9789b7c13
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-05-11 19:33:35 -0400

    Time: 486 ms (80.67%) | Memory: 21.2 MB (16.06%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,36 +1,33 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # ans = []
-        # for i in range(0, len(nums)-1):
-        #     k = i+1
-        #     for j in range(i+1, len(nums)):
-        #         s = [nums[i]]
-        #         t = nums[i] + nums[j]
-        #         if 0 - t in nums[k+1:]:
-        #             s.append(nums[j])
-        #             s.append(0-t)
-        #         s.sort()
-        #         if s not in ans and len(s) == 3:
-        #             ans.append(s)
-        #         k = k + 1
-        # return ans
         nums.sort()
-        ans = []
-        s = set()
-        for i in range(len(nums)):
-            l = i + 1
-            r = len(nums) - 1
+        res = []
+        def twoSum(l,r,target):
             while l < r:
-                if nums[l] + nums[r] + nums[i] == 0:
-                    s.add((nums[i], nums[l], nums[r]))
+                summ = nums[l] + nums[r]
+                if summ == target:
+                    res.append([-target, nums[l], nums[r]])
+                    # Skip duplicates
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
                     l += 1
                     r -= 1
-                elif nums[l] + nums[r] + nums[i] > 0:
-                    r -= 1
+                elif summ < target:
+                    l += 1
                 else:
-                    l += 1
-        for i in s:
-            ans.append(list(i))
-        return ans
+                    r -= 1
 
 
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            target = -nums[i]
+            l,r=i+1,len(nums)-1
+
+            twoSum(l,r,target)
+
+        return res
+
+            
