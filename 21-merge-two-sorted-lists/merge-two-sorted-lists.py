6833df52189f86c516f17c7f5010ff2978cc0914
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2025-05-11 20:07:41 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (61.76%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,31 +5,23 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        p1 = list1
-        p2 = list2
-        head = ListNode()
-        p3 = head
+        mergedList = ListNode(-1)
 
-        while (p1 and p2):
-            if p1.val == p2.val:
-                p3.next = ListNode(p1.val)
-                p3=p3.next
-                p3.next= ListNode(p2.val)
-                p3=p3.next
-                p1 = p1.next
-                p2 = p2.next
-            elif p1.val < p2.val:
-                p3.next = ListNode(p1.val)
-                p1 = p1.next
-                p3 = p3.next
-            elif p2.val < p1.val:
-                p3.next = ListNode(p2.val)
-                p2 = p2.next
-                p3 = p3.next
-        if p1:
-            p3.next = p1
-        if p2:
-            p3.next = p2
-        head = head.next
-        return head
-                
+        prev = mergedList
+
+        while list1 and list2:
+            if list1.val<=list2.val:
+                prev.next = ListNode(list1.val)
+                prev = prev.next
+                list1 = list1.next
+            else:
+                prev.next = ListNode(list2.val)
+                prev = prev.next
+                list2 = list2.next
+
+        if list1:
+            prev.next = list1
+        if list2:
+            prev.next = list2
+
+        return mergedList.next
