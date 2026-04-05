9191467198b40a307d482ead222aac78c70bb22d
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2026-04-05 18:20:49 -0400

    Time: 251 ms (42.88%) | Memory: 21.6 MB (58.07%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -3,40 +3,35 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
 
-
         def bfs(r,c):
             queue = deque([(r, c)])
             grid[r][c] = "2"
-            size = 1
-
+            size =1 
             while queue:
                 dr,dc = queue.popleft()
-                
-                for row,col in [(-1,0),(1,0),(0,-1),(0,1)]:
-                    nr,nc = dr+row,dc+col
+                for rw,cm in [(-1,0),(1,0),(0,-1),(0,1)]:
+                    nr,nc = dr+rw,dc+cm
                     if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]=="1":
                         grid[nr][nc]="2"
-                        queue.append([nr,nc])
                         size+=1
-            return size 
-
-        def dfs(r,c):
-            if r<0 or r>=rows or c<0 or c>=cols or grid[r][c]!="1":
-                return 0
-            
-            grid[r][c] = "2"
-            size = 1
-
-            for dr,dc in [(-1,0),(1,0),(0,1),(0,-1)]:
-                size+=dfs(r+dr,c+dc)
+                        queue.append([nr,nc])
             return size
 
+        def dfs(r,c):
+            if c<0 or r<0 or c>=cols or r>=rows or grid[r][c]!="1":
+                return 0
+            grid[r][c]="2"
+            size = 1 
+            for row,col in [(-1,0),(0,-1),(1,0),(0,1)]:
+                size+= dfs(r+row,c+col)
+            return size
+            
         count = 0
         sizes = []
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c]=="1":
-                    count+=1
-                    sizes.append(bfs(r,c))
-                    # sizes.append(dfs(r,c))
+                if grid[r][c] == "1":
+                    count += 1
+                    #sizes.append(bfs(r, c))
+                    sizes.append(dfs(r,c))
         return count
