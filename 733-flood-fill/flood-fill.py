da3d737af496f13a8527287fc2db95560bc0f67f
Parth Patel <56263875+pparth9199@users.noreply.github.com>
2026-04-05 18:33:17 -0400

    Time: 0 ms (100.00%) | Memory: 19.8 MB (22.01%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -3,6 +3,19 @@ class Solution:
         rows = len(image)
         cols = len(image[0])
         before = image[sr][sc]
+
+        def bfs(r,c):
+            queue = deque([(r,c)])
+            image[r][c]=color
+
+            while queue:
+                dr,dc = queue.popleft()
+                for row,col in ([(-1,0),(0,1),(1,0),(0,-1)]):
+                    nr,nc = dr+row,dc+col
+                    if 0<=nr<rows and 0<=nc<cols and image[nr][nc]==before and image[nr][nc]!=color:
+                        image[nr][nc]=color
+                        queue.append([nr,nc])
+                
         def dfs(r,c):
             if r<0 or r>=rows or c<0 or c>=cols or image[r][c]!=before or image[r][c]==color:
                 return
@@ -11,5 +24,5 @@ class Solution:
             for rw,clm in ([(-1,0),(1,0),(0,-1),(0,1)]):
                 dfs(r+rw,c+clm)
 
-        dfs(sr,sc)
+        bfs(sr,sc)
         return image
